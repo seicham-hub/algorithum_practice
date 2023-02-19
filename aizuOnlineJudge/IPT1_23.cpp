26fc82e9ef6d509825e1a76d00b8496cd57a6c2e
seicham <onigirimentai422@gmail.com>
2023-02-19 19:19:54 +0900

    23コミット

--- aizuOnlineJudge/IPT1_23.cpp
+++ aizuOnlineJudge/IPT1_23.cpp
@@ -1,34 +1,72 @@
 //Finding Missing Cards
 // 0214もう一度
 
-# include <iostream>
+# include <stdio.h>
+
 using namespace std;
 
 int main(){
-    int n,tmp,S[13],H[13],C[13],D[13];
     char mark;
+    int n,S[14],H[14],C[14],D[14],tmp;
+    for(int i=0;i<14;i++) S[i] =H[i] =C[i] =D[i] =0;
+    scanf("%d",&n);
 
-    for (int i=0;i<13;i++){
-        S[i] = H[i] = C[i] = D[i] =false;
+
+    for (int i=0;i<n;i++){
+        scanf("%c %d",&mark,&n);
+        
     }
-
-    cin >> n;
-
-
-    for (int i =0;i<n;i++){
-        cin >> mark >> tmp;
-
-        switch(mark){
-            case 'S':
-                S[tmp] =true;
-            case 'H':
-                H[tmp] =true;
-            case 'C':
-                C[tmp] =true;
-            case 'D':
-                D[tmp] =true;
-        }
-    }
-
-    return 0;
 }
+
+// # include <iostream>
+// using namespace std;
+
+// int main(){
+//     int n,tmp,S[14],H[14],C[14],D[14];
+//     char mark;
+
+//     for (int i=0;i<14;i++){
+//         S[i] =0;
+//         H[i] =0;
+//         C[i] = 0;
+//         D[i] = 0;
+//     }
+
+//     cin >> n;
+
+
+//     for (int i =0;i<n;i++){
+//         cin >> mark >> tmp;
+
+//         switch(mark){
+//             case 'S':
+//                 S[tmp] =1;
+//                 break;
+//             case 'H':
+//                 H[tmp] =1;
+//                 break;
+//             case 'C':
+//                 C[tmp] =1;
+//                 break;
+//             case 'D':
+//                 D[tmp] =1;
+//                 break;
+//         }
+//     }
+
+
+//     for (int i=1;i<14;i++){
+//         if (!S[i]) cout << "S" << " " << i << endl;
+//     }
+//     for (int i=1;i<14;i++){
+//         if (!H[i]) cout << "H" << " " << i << endl;
+//     }
+//     for (int i=1;i<14;i++){
+//         if (!C[i]) cout << "C" << " " << i << endl;
+//     }
+//     for (int i=1;i<14;i++){
+//         if (!D[i]) cout << "D" << " " << i << endl;
+//     }
+
+//     return 0;
+// }
