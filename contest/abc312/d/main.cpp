9666e8fe2405ef688c3353fc234538ce554140d5
seicham <onigirimentai422@gmail.com>
2023-08-02 08:53:41 +0900

    2023＿0801

--- contest/abc312/d/main.cpp
+++ contest/abc312/d/main.cpp
@@ -3,39 +3,78 @@
 // モジュロ演算　(A+B)%C = (A%C+B%C)%C 剰余定理で証明可
 
 // 0730もう一度
+// 0801もう一度
 
 /*
-解答のコード
+8/1やり直し分
 */
+
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
 
 int main()
 {
-
     string s;
     cin >> s;
+
     int n = s.size();
 
-    vector<vector<long long>> dp(n + 1, vector<long long>(n + 1));
+    // dp[i][j]  i番目　j :( - ) の数
+    vector<vector<long long>> dp(n + 1, vector<long long>(n + 1, 0));
+
     dp[0][0] = 1;
 
     rep(i, n)
     {
         rep(j, n)
         {
+
             if (s[i] != ')')
                 dp[i + 1][j + 1] += dp[i][j] % 998244353;
+
             if (j != 0 && s[i] != '(')
                 dp[i + 1][j - 1] += dp[i][j] % 998244353;
         }
     }
 
-    cout << (long long)(dp[n][0] % 998244353) << endl;
+    cout << dp[n][0] % 998244353 << endl;
 
     return 0;
 }
+
+/*
+解答のコード
+*/
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+
+// int main()
+// {
+
+//     string s;
+//     cin >> s;
+//     int n = s.size();
+
+//     vector<vector<long long>> dp(n + 1, vector<long long>(n + 1));
+//     dp[0][0] = 1;
+
+//     rep(i, n)
+//     {
+//         rep(j, n)
+//         {
+//             if (s[i] != ')')
+//                 dp[i + 1][j + 1] += dp[i][j] % 998244353;
+//             if (j != 0 && s[i] != '(')
+//                 dp[i + 1][j - 1] += dp[i][j] % 998244353;
+//         }
+//     }
+
+//     cout << (long long)(dp[n][0] % 998244353) << endl;
+
+//     return 0;
+// }
 
 // #include <bits/stdc++.h>
 // using namespace std;
