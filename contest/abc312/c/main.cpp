b3555643f48c2175eed29db22896e656f8a07ee8
seicham <onigirimentai422@gmail.com>
2023-08-01 09:28:23 +0900

    2023_0731

--- contest/abc312/c/main.cpp
+++ contest/abc312/c/main.cpp
@@ -1,15 +1,115 @@
 // https://atcoder.jp/contests/abc312/tasks/abc312_c
 // 最大のxを考えるとき、条件を満たす数式がxによって単調増加、単調減少なら二分探索か
-// 解けなかった... なぜ下記で間違ってるのか分からない
+// 解けなかった... なぜ下記で間違ってるのか分からない →間隔に頼りすぎた
+
+// 0730もう一度
+// 0801もう一度
+
+/*
+7/30やり直し分
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
 
+int n, m;
+vector<int> a, b;
+
+bool check(int mid)
+{
+    auto sIt = upper_bound(a.begin(), a.end(), mid);
+    int sellP = sIt - a.begin();
+
+    auto bIt = lower_bound(b.begin(), b.end(), mid);
+    int buyP = m - (bIt - b.begin());
+
+    return sellP >= buyP;
+}
+
 int main()
 {
+
+    cin >> n >> m;
+
+    a.resize(n);
+    b.resize(m);
+
+    rep(i, n) cin >> a[i];
+    rep(i, m) cin >> b[i];
+
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+
+    int left = 0;
+    int right = 1e9 + 1;
+
+    while (abs(right - left) > 1)
+    {
+        int mid = (left + right) / 2;
+        if (check(mid))
+            right = mid;
+        else
+            left = mid;
+    }
+
+    cout << right << endl;
+
     return 0;
 }
+
+/*
+解答のコード
+*/
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+
+// int n, m, ans = 0;
+// vector<int> a, b;
+
+// bool check(int mid)
+// {
+
+//     auto selIt = upper_bound(a.begin(), a.end(), mid);
+//     int selNum = selIt - a.begin();
+
+//     auto buyIt = lower_bound(b.begin(), b.end(), mid);
+//     int buyNum = m - (buyIt - b.begin());
+
+//     return (selNum - buyNum >= 0);
+// }
+
+// int main()
+// {
+
+//     cin >> n >> m;
+
+//     a.resize(n);
+//     b.resize(m);
+
+//     rep(i, n) cin >> a[i];
+//     rep(i, m) cin >> b[i];
+
+//     sort(a.begin(), a.end());
+//     sort(b.begin(), b.end());
+
+//     int left = -1;
+//     int right = 1e9 + 1;
+
+//     while (abs(right - left > 1))
+//     {
+//         int mid = (right + left) / 2;
+
+//         if (check(mid))
+//             right = mid;
+//         else
+//             left = mid;
+//     }
+
+//     cout << right << endl;
+//     return 0;
+// }
 
 /*
 本番のコード
--- contest/abc312/d/main.cpp
+++ contest/abc312/d/main.cpp
@@ -1,35 +1,74 @@
+// https://atcoder.jp/contests/abc312/tasks/abc312_d
+// 分からなかった　dpという発想はどこから？→法則が分かりにくいから、前の状態から考えよう
+// モジュロ演算　(A+B)%C = (A%C+B%C)%C 剰余定理で証明可
+
+// 0730もう一度
+
+/*
+解答のコード
+*/
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
-
-long long ans = 0;
-
-void dfs(int i, string s)
-{
-
-    if (i == s.size() - 1)
-    {
-    }
-
-    if (s[i] == '?')
-    {
-        s[i] = ')';
-        dfs(i + 1, s);
-        s[i] = '(';
-        dfs(i + 1, s);
-
-        s[i] = '?';
-    }
-}
 
 int main()
 {
 
     string s;
-
     cin >> s;
+    int n = s.size();
 
-    dfs(0, s);
+    vector<vector<long long>> dp(n + 1, vector<long long>(n + 1));
+    dp[0][0] = 1;
+
+    rep(i, n)
+    {
+        rep(j, n)
+        {
+            if (s[i] != ')')
+                dp[i + 1][j + 1] += dp[i][j] % 998244353;
+            if (j != 0 && s[i] != '(')
+                dp[i + 1][j - 1] += dp[i][j] % 998244353;
+        }
+    }
+
+    cout << (long long)(dp[n][0] % 998244353) << endl;
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+
+// long long ans = 0;
+
+// void dfs(int i, string s)
+// {
+
+//     if (i == s.size() - 1)
+//     {
+//     }
+
+//     if (s[i] == '?')
+//     {
+//         s[i] = ')';
+//         dfs(i + 1, s);
+//         s[i] = '(';
+//         dfs(i + 1, s);
+
+//         s[i] = '?';
+//     }
+// }
+
+// int main()
+// {
+
+//     string s;
+
+//     cin >> s;
+
+//     dfs(0, s);
+
+//     return 0;
+// }
