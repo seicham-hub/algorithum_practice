c8e93edad7cf6ea578c624cc29d7910dea43a15e
seicham <onigirimentai422@gmail.com>
2023-08-30 09:01:21 +0900

    2023_0829

--- contest/abc314/d/main.cpp
+++ contest/abc314/d/main.cpp
@@ -1,6 +1,171 @@
 // https://atcoder.jp/contests/abc314/tasks/abc314_d
 // 毎回forで回してたら終わらない..→最後にまとめてできないか？
 // 0822もう一度
+// 0825もう一度
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+
+int main()
+{
+    int n, q;
+    string s;
+
+    cin >> n >> s >> q;
+
+    vector<pair<int, char>> personal(n); // personal[sの何文字目か] = {時刻,変更後の文字}
+
+    rep(i, n) personal[i] = {0, s[i]};
+
+    pair<int, int> full = {-100, 5}; // {時刻、大文字か小文字か(2,3)}
+
+    rep(i, q)
+    {
+
+        int t, x;
+        char c;
+        cin >> t >> x >> c;
+
+        if (t == 1)
+            personal[x - 1] = {i, c};
+        else if (t == 2)
+            full = {i, t};
+        else
+            full = {i, t};
+    }
+
+    for (auto p : personal)
+    {
+        int time = p.first;
+        char mozi = p.second;
+
+        int ftime = full.first;
+        int to = full.second;
+
+        if (ftime < time)
+            cout << mozi;
+        else if (to == 2)
+            cout << (char)tolower(mozi);
+        else
+            cout << (char)toupper(mozi);
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+
+// int main()
+// {
+
+//     int N;
+//     string S;
+//     cin >> N >> S;
+
+//     vector<pair<int, char>> status(N); // 個別の操作（最終変更時間、現在の文字)
+
+//     rep(i, N)
+//     {
+//         status[i] = {0, S[i]};
+//     }
+
+//     int Q;
+//     cin >> Q;
+
+//     optional<pair<int, int>> fill = nullopt; // 全体に対する操作（最終変更時間、最後の変更のタイプ）
+
+//     rep(i, Q)
+//     {
+//         int t, x;
+//         char c;
+
+//         cin >> t >> x >> c;
+//         if (t == 1)
+//             status[x - 1] = {i, c};
+//         else
+//             fill = {i, t};
+//     }
+
+//     for (const auto &[time, c] : status)
+//     {
+//         if (!fill || time >= fill->first)
+//             cout << c;
+//         else if (fill->second == 2)
+//             cout << static_cast<char>(tolower(c));
+//         else
+//             cout << static_cast<char>(toupper(c));
+//     }
+
+//     cout << endl;
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+
+// int main()
+// {
+//     int n, q;
+//     string s;
+//     cin >> n >> s >> q;
+
+//     vector<pair<char, int>> exchange(n + 1);
+
+//     rep(i, s.size())
+//     {
+//         exchange[i] = {s[i], 0};
+//     }
+
+//     // この位置で全部文字変換
+//     pair<int, char> changeCase = {-100, 'd'};
+
+//     rep(i, q)
+//     {
+//         int t, x;
+//         char c;
+//         cin >> t >> x >> c;
+//         x--;
+
+//         if (t == 1)
+//         {
+//             exchange[x] = {c, i};
+//         }
+//         else if (t == 2)
+//         {
+//             changeCase = {i, 'l'};
+//         }
+//         else if (t == 3)
+//         {
+//             changeCase = {i, 'u'};
+//         }
+//     }
+
+//     string ans = "";
+
+//     rep(i, s.size())
+//     {
+//         if (changeCase.first < exchange[i].second)
+//         {
+//             ans += exchange[i].first;
+//         }
+//         else
+//         {
+//             if (changeCase.second == 'l')
+//                 ans += tolower(exchange[i].first);
+//             else
+//                 ans += toupper(exchange[i].first);
+//         }
+//     }
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
 
 /*
 自分の解答
--- contest/abc315/c/main.cpp
+++ contest/abc315/c/main.cpp
@@ -3,6 +3,7 @@
 // 基本美味しさがでかいやつから選べばよいが
 
 // 0823もう一度
+// 0829もう一度
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -11,44 +12,85 @@ using namespace std;
 int main()
 {
     int n;
-    cin >> n;
-    vector<vector<int>> bk(n + 1);
 
+    cin >> n;
+    vector<vector<int>> fs(n + 1, vector<int>(2, 0));
     rep(i, n)
     {
         int f, s;
         cin >> f >> s;
-        bk[f].push_back(s);
+
+        fs[f].push_back(s);
     }
-
-    int res = 0;
-    vector<int> best;
-
     for (int i = 1; i <= n; i++)
     {
-        sort(bk[i].begin(), bk[i].end());
-        reverse(bk[i].begin(), bk[i].end());
-
-        if (bk[i].size() >= 2)
-        {
-            res = max(res, bk[i][0] + bk[i][1] / 2);
-        }
-        if (bk[i].size() >= 1)
-        {
-            best.push_back(bk[i][0]);
-        }
+        sort(fs[i].begin(), fs[i].end());
+        reverse(fs[i].begin(), fs[i].end());
     }
 
-    sort(best.begin(), best.end());
-    reverse(best.begin(), best.end());
+    sort(fs.begin(), fs.end());
+    reverse(fs.begin(), fs.end());
 
-    if (best.size() >= 2)
-        res = max(res, best[0] + best[1]);
+    int ans = 0;
 
-    cout << res << endl;
+    ans = fs[0][0] + fs[0][1] / 2;
+
+    ans = max(ans, fs[0][0] + fs[1][0]);
+
+    cout << ans << endl;
 
     return 0;
 }
+
+/*
+解答のコード
+*/
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<vector<int>> bk(n + 1);
+
+//     rep(i, n)
+//     {
+//         int f, s;
+//         cin >> f >> s;
+//         bk[f].push_back(s);
+//     }
+
+//     int res = 0;
+//     vector<int> best;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         sort(bk[i].begin(), bk[i].end());
+//         reverse(bk[i].begin(), bk[i].end());
+
+//         if (bk[i].size() >= 2)
+//         {
+//             res = max(res, bk[i][0] + bk[i][1] / 2);
+//         }
+//         if (bk[i].size() >= 1)
+//         {
+//             best.push_back(bk[i][0]);
+//         }
+//     }
+
+//     sort(best.begin(), best.end());
+//     reverse(best.begin(), best.end());
+
+//     if (best.size() >= 2)
+//         res = max(res, best[0] + best[1]);
+
+//     cout << res << endl;
+
+//     return 0;
+// }
 
 /*
 自分で解いたコード
