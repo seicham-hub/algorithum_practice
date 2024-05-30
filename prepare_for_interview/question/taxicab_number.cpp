9a51a4d337cfa5f1935c8da1425ed916841adbfa
seicham <onigirimentai422@gmail.com>
2024-05-30 09:44:58 +0900

    2024_05_29

--- prepare_for_interview/question/taxicab_number.cpp
+++ prepare_for_interview/question/taxicab_number.cpp
@@ -3,70 +3,119 @@
 // Input 2, 2 => [(1729,[(1,12),(9,10)]),(4104,[(2,16),(9,15)])]
 // Input 1,3 => [(87539319,[(167,436),(228,423),(255,414)])]
 
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <unordered_map>
-#include <utility> // for std::pair
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<pair<int, vector<pair<int, int>>>> taxi_cab_number(int max_results_num, int required_pairs_num = 2)
+vector<pair<int, vector<pair<int, int>>>> taxi_cab_num(int max_results_num, int max_required_pairs_num)
 {
     vector<pair<int, vector<pair<int, int>>>> results;
-    int current_number = 1;
 
-    while (results.size() < max_results_num)
+    int num = 1;
+    while (true)
     {
-        unordered_map<int, vector<pair<int, int>>> pairs;
-        int max_param = static_cast<int>(pow(current_number, 1.0 / 3.0)) + 1;
 
-        for (int x = 1; x < max_param; ++x)
+        vector<pair<int, int>> tmp_pairs;
+        for (int x = 1; x < pow(num, 1.0 / 3.0) + 1; x++)
         {
-            for (int y = x + 1; y < max_param; ++y)
+            for (int y = x + 1; y <= pow(num, 1.0 / 3.0) + 1; y++)
             {
-                if (x * x * x + y * y * y == current_number)
-                {
-                    pairs[current_number].emplace_back(x, y);
-                }
+                if (x * x * x + y * y * y == num)
+                    tmp_pairs.push_back({x, y});
             }
         }
+        if (tmp_pairs.size() == max_required_pairs_num)
+            results.push_back({num, tmp_pairs});
 
-        if (pairs[current_number].size() == required_pairs_num)
-        {
-            results.emplace_back(current_number, pairs[current_number]);
-        }
-
-        ++current_number;
+        if (results.size() == max_results_num)
+            break;
+        num++;
     }
 
     return results;
 }
 
-void print_taxi_cab_numbers(const vector<pair<int, vector<pair<int, int>>>> &results)
-{
-    for (const auto &result : results)
-    {
-        cout << "Number: " << result.first << " Pairs: ";
-        for (const auto &pair : result.second)
-        {
-            cout << "(" << pair.first << ", " << pair.second << ") ";
-        }
-        cout << endl;
-    }
-}
-
 int main()
 {
-    int max_results_num = 1;
-    int required_pairs_num = 3;
 
-    vector<pair<int, vector<pair<int, int>>>> results = taxi_cab_number(max_results_num, required_pairs_num);
-    print_taxi_cab_numbers(results);
+    vector<pair<int, vector<pair<int, int>>>> ans = taxi_cab_num(2, 2);
+
+    for (auto line : ans)
+    {
+        cout << "(" << line.first << ", [";
+
+        for (auto p : line.second)
+            cout << "(" << p.first << "," << p.second << "), ";
+
+        cout << "])" << endl;
+    }
 
     return 0;
 }
+
+// #include <iostream>
+// #include <vector>
+// #include <cmath>
+// #include <unordered_map>
+// #include <utility> // for std::pair
+// #include <algorithm>
+
+// using namespace std;
+
+// vector<pair<int, vector<pair<int, int>>>> taxi_cab_number(int max_results_num, int required_pairs_num = 2)
+// {
+//     vector<pair<int, vector<pair<int, int>>>> results;
+//     int current_number = 1;
+
+//     while (results.size() < max_results_num)
+//     {
+//         unordered_map<int, vector<pair<int, int>>> pairs;
+//         int max_param = static_cast<int>(pow(current_number, 1.0 / 3.0)) + 1;
+
+//         for (int x = 1; x < max_param; ++x)
+//         {
+//             for (int y = x + 1; y < max_param; ++y)
+//             {
+//                 if (x * x * x + y * y * y == current_number)
+//                 {
+//                     pairs[current_number].emplace_back(x, y);
+//                 }
+//             }
+//         }
+
+//         if (pairs[current_number].size() == required_pairs_num)
+//         {
+//             results.emplace_back(current_number, pairs[current_number]);
+//         }
+
+//         ++current_number;
+//     }
+
+//     return results;
+// }
+
+// void print_taxi_cab_numbers(const vector<pair<int, vector<pair<int, int>>>> &results)
+// {
+//     for (const auto &result : results)
+//     {
+//         cout << "Number: " << result.first << " Pairs: ";
+//         for (const auto &pair : result.second)
+//         {
+//             cout << "(" << pair.first << ", " << pair.second << ") ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int max_results_num = 1;
+//     int required_pairs_num = 3;
+
+//     vector<pair<int, vector<pair<int, int>>>> results = taxi_cab_number(max_results_num, required_pairs_num);
+//     print_taxi_cab_numbers(results);
+
+//     return 0;
+// }
 
 // #include <bits/stdc++.h>
 // using namespace std;
---
+++ prepare_for_interview/question/tower_of_hanoi.cpp
@@ -0,0 +1,43 @@
+// ハノイの塔を実装しろ
+// move 1 from A to C
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void hanoi(int disk, int current, int dest, int center)
+{
+
+    if (disk < 1)
+        return;
+
+    hanoi(disk - 1, current, center, dest);
+    cout << "move" << disk << "from" << (char)current << "to" << (char)dest << endl;
+    hanoi(disk - 1, center, dest, current);
+}
+
+int main()
+{
+
+    hanoi(3, 'A', 'C', 'B');
+
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void hanoi(int disc, char current, char dest, char center)
+// {
+//     if (disc < 1)
+//         return;
+//     hanoi(disc - 1, current, center, dest);
+//     cout << "move" << disc << "from" << current << "to" << dest << endl;
+//     hanoi(disc - 1, center, dest, current);
+// }
+
+// int main()
+// {
+//     hanoi(3, 'A', 'C', 'B');
+
+//     return 0;
+// }
