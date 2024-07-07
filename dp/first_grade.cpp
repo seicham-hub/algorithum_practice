d3f96f04b1d011c8b400f53d1df491d3d3731b2b
seicham <onigirimentai422@gmail.com>
2024-07-08 00:11:27 +0900

    2024_07_05

--- dp/first_grade.cpp
+++ dp/first_grade.cpp
@@ -1,6 +1,7 @@
-// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_10_C&lang=ja
+// https://atcoder.jp/contests/joi2011yo/tasks/joi2011yo_d
 
 // 2024_07_06もう一度
+// 2024_07_08もう一度
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -10,30 +11,64 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> nums(n);
-
-    for (int i = 0; i < n; i++)
+    vector<int> nums(n + 1);
+    for (int i = 1; i <= n; i++)
         cin >> nums[i];
 
     vector<vector<long long>> dp(n, vector<long long>(21, 0));
-    dp[1][nums[0]] = 1;
 
-    for (int i = 2; i < n; i++)
+    dp[1][nums[1]] = 1;
+
+    for (int i = 2; i <= n - 1; i++)
     {
         for (int j = 0; j <= 20; j++)
         {
-            if (j - nums[i - 1] >= 0 && j + nums[i - 1] <= 20)
-                dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j + nums[i - 1]];
-            else if (j - nums[i - 1] >= 0)
-                dp[i][j] = dp[i - 1][j - nums[i - 1]];
-            else if (j + nums[i - 1] <= 20)
-                dp[i][j] = dp[i - 1][j + nums[i - 1]];
-            else
-                dp[i][j] = 0;
+
+            if (0 <= j - nums[i] && j + nums[i] <= 20)
+                dp[i][j] = dp[i - 1][j - nums[i]] + dp[i - 1][j + nums[i]];
+            else if (0 <= j - nums[i])
+                dp[i][j] = dp[i - 1][j - nums[i]];
+            else if (j + nums[i] <= 20)
+                dp[i][j] = dp[i - 1][j + nums[i]];
         }
     }
 
-    cout << dp[n - 1][nums[n - 1]] << endl;
+    cout << dp[n - 1][nums.back()] << endl;
 
     return 0;
 }
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     vector<int> nums(n);
+
+//     for (int i = 0; i < n; i++)
+//         cin >> nums[i];
+
+//     vector<vector<long long>> dp(n, vector<long long>(21, 0));
+//     dp[1][nums[0]] = 1;
+
+//     for (int i = 2; i < n; i++)
+//     {
+//         for (int j = 0; j <= 20; j++)
+//         {
+//             if (j - nums[i - 1] >= 0 && j + nums[i - 1] <= 20)
+//                 dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j + nums[i - 1]];
+//             else if (j - nums[i - 1] >= 0)
+//                 dp[i][j] = dp[i - 1][j - nums[i - 1]];
+//             else if (j + nums[i - 1] <= 20)
+//                 dp[i][j] = dp[i - 1][j + nums[i - 1]];
+//             else
+//                 dp[i][j] = 0;
+//         }
+//     }
+
+//     cout << dp[n - 1][nums[n - 1]] << endl;
+
+//     return 0;
+// }
--- dp/pasta.cpp
+++ dp/pasta.cpp
@@ -1,6 +1,142 @@
 // https://atcoder.jp/contests/joi2012yo/tasks/joi2012yo_d
 
 // 2024_07_06もう一度
+// 2024_07_08もう一度
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MOD = 10000;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+
+    unordered_map<int, int> pre;
+
+    for (int i = 0; i < k; i++)
+    {
+        int day, taste;
+        cin >> day >> taste;
+
+        pre[day] = taste;
+    }
+
+    vector<vector<vector<long long>>> dp(n + 1, vector<vector<long long>>(3, vector<long long>(3, 0)));
+    dp[0][0][0] = 1;
+
+    for (int day = 1; day <= n; ++day)
+    {
+        for (int cur = 0; cur < 3; ++cur)
+        {
+            for (int prev = 0; prev < 3; ++prev)
+            {
+                for (int prevprev = 0; prevprev < 3; ++prevprev)
+                {
+
+                    if (day != 1 && day != 2 && cur == prev && prev == prevprev)
+                        continue;
+
+                    if (pre[day])
+                    {
+                        if (pre[day] - 1 == cur)
+                            dp[day][cur][prev] = (dp[day][cur][prev] + dp[day - 1][prev][prevprev]) % MOD;
+                    }
+                    else
+                    {
+                        dp[day][cur][prev] = (dp[day][cur][prev] + dp[day - 1][prev][prevprev]) % MOD;
+                    }
+                }
+            }
+        }
+    }
+
+    long long ans = 0;
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            ans = (ans + dp[n][i][j]) % MOD;
+
+    cout << ans << endl;
+
+    return 0;
+}
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// const int MOD = 10000;
+
+// int main()
+// {
+//     int N, K;
+//     cin >> N >> K;
+//     vector<int> pasta(N + 1, 0); // 各日付のパスタの種類を格納
+
+//     for (int i = 0; i < K; ++i)
+//     {
+//         int A, B;
+//         cin >> A >> B;
+//         pasta[A] = B; // A日目のパスタはBに決定
+//     }
+
+//     // DPテーブルの初期化
+//     vector<vector<vector<int>>> dp(N + 1, vector<vector<int>>(4, vector<int>(4, 0)));
+
+//     // 初日の設定
+//     if (pasta[1] != 0)
+//     {
+//         dp[1][pasta[1]][0] = 1;
+//     }
+//     else
+//     {
+//         for (int i = 1; i <= 3; ++i)
+//         {
+//             dp[1][i][0] = 1;
+//         }
+//     }
+
+//     // DPの遷移
+//     for (int day = 2; day <= N; ++day)
+//     {
+//         for (int prev = 1; prev <= 3; ++prev)
+//         {
+//             for (int prevprev = 0; prevprev <= 3; ++prevprev)
+//             {
+//                 if (pasta[day] != 0)
+//                 {
+//                     if (pasta[day] != prev || pasta[day] != prevprev)
+//                     {
+//                         dp[day][pasta[day]][prev] = (dp[day][pasta[day]][prev] + dp[day - 1][prev][prevprev]) % MOD;
+//                     }
+//                 }
+//                 else
+//                 {
+//                     for (int curr = 1; curr <= 3; ++curr)
+//                     {
+//                         if (curr != prev || curr != prevprev)
+//                         {
+//                             dp[day][curr][prev] = (dp[day][curr][prev] + dp[day - 1][prev][prevprev]) % MOD;
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     // 結果を集計
+//     int result = 0;
+//     for (int i = 1; i <= 3; ++i)
+//     {
+//         for (int j = 1; j <= 3; ++j)
+//         {
+//             result = (result + dp[N][i][j]) % MOD;
+//         }
+//     }
+
+//     cout << result << endl;
+//     return 0;
+// }
 
 // #include <bits/stdc++.h>
 // using namespace std;
