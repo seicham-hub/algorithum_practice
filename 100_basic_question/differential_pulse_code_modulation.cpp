66f8c0159423f09e70eec8351cc537e2e3f519a6
seicham <onigirimentai422@gmail.com>
2024-07-22 22:06:36 +0900

    2024_07_21

---
+++ 100_basic_question/differential_pulse_code_modulation.cpp
@@ -0,0 +1,132 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2199&lang=jp
+
+// 2024_07_22もう一度
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    while (true)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        if (!n && !m)
+            break;
+
+        vector<int> code_book(m);
+
+        for (int i = 0; i < m; i++)
+            cin >> code_book[i];
+
+        vector<int> x(n + 1);
+        for (int i = 1; i <= n; i++)
+            cin >> x[i];
+
+        vector<vector<int>> dp(n + 1, vector<int>(256, INT_MAX));
+        dp[0][128] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int y = 0; y <= 255; y++)
+            {
+                if (dp[i - 1][y] != INT_MAX)
+                {
+                    for (int cb : code_book)
+                    {
+                        int ny = y + cb;
+
+                        if (ny < 0)
+                            ny = 0;
+                        if (ny > 255)
+                            ny = 255;
+
+                        dp[i][ny] = min(dp[i][ny], dp[i - 1][y] + (x[i] - ny) * (x[i] - ny));
+                    }
+                }
+            }
+        }
+
+        int ans = INT_MAX;
+        for (int i = 0; i <= 255; i++)
+            ans = min(ans, dp[n][i]);
+
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int N, M;
+
+//     while (cin >> N >> M, N || M)
+//     {
+//         vector<int> C(M);
+//         for (int i = 0; i < M; i++)
+//         {
+//             cin >> C[i];
+//         }
+
+//         vector<int> x(N);
+//         for (int i = 0; i < N; i++)
+//         {
+//             cin >> x[i];
+//         }
+
+//         const int INF = 1e9;
+//         vector<vector<int>> dp(N + 1, vector<int>(256, INF));
+//         dp[0][128] = 0; // y0 = 128
+
+//         for (int i = 0; i < N; i++)
+//         {
+//             for (int y = 0; y < 256; y++)
+//             {
+//                 if (dp[i][y] == INF)
+//                     continue;
+//                 for (int j = 0; j < M; j++)
+//                 {
+//                     int ny = y + C[j];
+//                     if (ny < 0)
+//                         ny = 0;
+//                     if (ny > 255)
+//                         ny = 255;
+//                     dp[i + 1][ny] = min(dp[i + 1][ny], dp[i][y] + (ny - x[i]) * (ny - x[i]));
+//                 }
+//             }
+//         }
+
+//         int ans = INF;
+//         for (int y = 0; y < 256; y++)
+//         {
+//             ans = min(ans, dp[N][y]);
+//         }
+//         cout << ans << endl;
+//     }
+
+//     return 0;
+// }
+
+// 自分で書いたコード
+// 分からなかった
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+//     int n, m;
+//     cin >> n >> m;
+
+//     vector<int> code_book(n + 1);
+
+//     for (int i = 1; i <= m; i++)
+//         cin >> code_book[i];
+
+//     return 0;
+// }
--- 100_basic_question/pollocks_conjecture.cpp
+++ 100_basic_question/pollocks_conjecture.cpp
@@ -1,50 +1,102 @@
 // https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1167&lang=jp
 // 2024_07_21もう一度
+// 2024_07_22もう一度
 
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> create_dp(vector<int> &t)
+{
+
+    vector<int> dp(1e6 + 1, 1e6);
+
+    dp[0] = 0;
+
+    for (int i = 1; i < t.size(); i++)
+        for (int num = 1; num <= 1e6; num++)
+        {
+            if (num - t[i] >= 0)
+                dp[num] = min(dp[num], dp[num - t[i]] + 1);
+        }
+
+    return dp;
+}
+
 int main()
 {
 
-    vector<int> t_even, t_odd;
+    vector<int> t(1, 0), t_odd(1, 0);
+
     for (int i = 1; i <= 200; i++)
     {
-        int t = (i * (i + 1) * (i + 2)) / 6;
+        int ti = (i * (i + 1) * (i + 2)) / 6;
+        t.push_back(ti);
 
-        t_even.push_back(t);
-        if (t % 2 != 0)
-            t_odd.push_back(t);
+        if (ti % 2 == 1)
+            t_odd.push_back(ti);
     }
 
-    auto create_dp = [&](vector<int> &dp, vector<int> &t_s)
-    {
-        dp[0] = 0;
-
-        for (int t : t_s)
-        {
-            for (int j = t; j <= 1e6; j++)
-            {
-                dp[j] = min(dp[j], dp[j - t] + 1);
-            }
-        }
-    };
-
-    vector<int> dp_even(vector<int>(1e6 + 1, 1e6));
-    vector<int> dp_odd(vector<int>(1e6 + 1, 1e6));
-
-    create_dp(dp_even, t_even);
-    create_dp(dp_odd, t_odd);
+    vector<int> dp = create_dp(t);
+    vector<int> dp_odd = create_dp(t_odd);
 
     while (true)
     {
-        int num;
-        cin >> num;
-        if (!num)
+        int n;
+        cin >> n;
+
+        if (n == 0)
             break;
 
-        cout << dp_even[num] << " " << dp_odd[num] << endl;
+        cout << dp[n] << " " << dp_odd[n] << endl;
     }
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main()
+// {
+
+//     vector<int> t_even, t_odd;
+//     for (int i = 1; i <= 200; i++)
+//     {
+//         int t = (i * (i + 1) * (i + 2)) / 6;
+
+//         t_even.push_back(t);
+//         if (t % 2 != 0)
+//             t_odd.push_back(t);
+//     }
+
+//     auto create_dp = [&](vector<int> &dp, vector<int> &t_s)
+//     {
+//         dp[0] = 0;
+
+//         for (int t : t_s)
+//         {
+//             for (int j = t; j <= 1e6; j++)
+//             {
+//                 dp[j] = min(dp[j], dp[j - t] + 1);
+//             }
+//         }
+//     };
+
+//     vector<int> dp_even(vector<int>(1e6 + 1, 1e6));
+//     vector<int> dp_odd(vector<int>(1e6 + 1, 1e6));
+
+//     create_dp(dp_even, t_even);
+//     create_dp(dp_odd, t_odd);
+
+//     while (true)
+//     {
+//         int num;
+//         cin >> num;
+//         if (!num)
+//             break;
+
+//         cout << dp_even[num] << " " << dp_odd[num] << endl;
+//     }
+
+//     return 0;
+// }
