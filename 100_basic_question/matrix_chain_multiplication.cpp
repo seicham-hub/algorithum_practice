d1e1dda693d668b66abb83b0da47bc6492611557
seicham <onigirimentai422@gmail.com>
2024-08-10 12:54:27 +0900

    2024_08_10

--- 100_basic_question/matrix_chain_multiplication.cpp
+++ 100_basic_question/matrix_chain_multiplication.cpp
@@ -2,56 +2,102 @@
 
 // 分からなかった
 // 2024_08_08もう一度
+// 2024_08_11もう一度
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int matrixChainMultiplication(vector<int> &p, int n)
+int count_min(int &start, int &end, vector<pair<int, int>> &m, vector<vector<int>> &dp)
 {
-
-    vector<vector<int>> dp(n, vector<int>(n, 0));
-
-    // lは連鎖の長さ
-    for (int l = 1; l < n; l++)
+    int result = INT_MAX;
+    for (int i = start; i < end; i++)
     {
-        for (int i = 0; i <= n - 1 - l; i++)
-        {
-            int j = i + l;
-            dp[i][j] = INT_MAX;
-            for (int k = i; k <= j - 1; k++)
-            {
-                int q = dp[i][k] + dp[k + 1][j] + p[i] * p[k + 1] * p[j + 1];
-                if (q < dp[i][j])
-                {
-                    dp[i][j] = q;
-                }
-            }
-        }
+        result = min(result, dp[start][i] + dp[i + 1][end] + m[start].first * m[i].second * m[end].second);
     }
 
-    return dp[0][n - 1];
+    return result;
 }
 
 int main()
 {
+
     int n;
     cin >> n;
 
-    vector<int> p(n + 1);
-    vector<int> r(n), c(n);
+    vector<pair<int, int>> m(n + 1);
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        cin >> r[i] >> c[i];
-        p[i] = r[i];
-        if (i == n - 1)
-            p[i + 1] = c[i];
+        cin >> m[i].first >> m[i].second;
     }
 
-    cout << matrixChainMultiplication(p, n) << endl;
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, INT_MAX));
+    for (int i = 0; i <= n; i++)
+        dp[i][i] = 0;
+
+    for (int l = 1; l <= n; l++)
+    {
+        for (int i = 1; i <= n - l + 1; i++)
+        {
+            int j = i + l - 1;
+
+            dp[i][j] = min(dp[i][j], count_min(i, j, m, dp));
+        }
+    }
+
+    cout << dp[1][n] << endl;
 
     return 0;
 }
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int matrixChainMultiplication(vector<int> &p, int n)
+// {
+
+//     vector<vector<int>> dp(n, vector<int>(n, 0));
+
+//     // lは連鎖の長さ
+//     for (int l = 1; l < n; l++)
+//     {
+//         for (int i = 0; i <= n - 1 - l; i++)
+//         {
+//             int j = i + l;
+//             dp[i][j] = INT_MAX;
+//             for (int k = i; k <= j - 1; k++)
+//             {
+//                 int q = dp[i][k] + dp[k + 1][j] + p[i] * p[k + 1] * p[j + 1];
+//                 if (q < dp[i][j])
+//                 {
+//                     dp[i][j] = q;
+//                 }
+//             }
+//         }
+//     }
+
+//     return dp[0][n - 1];
+// }
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     vector<int> p(n + 1);
+//     vector<int> r(n), c(n);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> r[i] >> c[i];
+//         p[i] = r[i];
+//         if (i == n - 1)
+//             p[i + 1] = c[i];
+//     }
+
+//     cout << matrixChainMultiplication(p, n) << endl;
+
+//     return 0;
+// }
 
 // #include <bits/stdc++.h>
 // using namespace std;
