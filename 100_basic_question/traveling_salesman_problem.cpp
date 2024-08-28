4601fcffbfc0f48a91857c2600a1e1c62ff6ebfb
seicham <onigirimentai422@gmail.com>
2024-08-28 22:08:07 +0900

    2024_08_24

--- 100_basic_question/traveling_salesman_problem.cpp
+++ 100_basic_question/traveling_salesman_problem.cpp
@@ -1,48 +1,65 @@
 // https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DPL_2_A&lang=ja
 
+// 2024_08_28もう一度
+
 #include <bits/stdc++.h>
 using namespace std;
 
-// void dfs(int now, vector<int> dp, vector<bool> visited, vector<vector<pair<int, int>>> &graph)
-// {
-//     visited[now] = true;
+int calc_min_diff(int &start, int v, map<pair<int, int>, int> &graph)
+{
+    vector<vector<int>> dp(1 << v, vector<int>(v, INT_MAX));
+    dp[0][0] = 0;
+    dp[1 << start][start] = 0;
+    for (int msk = (1 << start); msk < (1 << v); msk++)
+    {
+        for (int i = 0; i < v; i++)
+        {
+            // int before = msk - pow(2, i);
+            // if (before < 0)
+            //     continue;
 
-//     for (auto next : graph[now])
-//     {
+            int before = msk & ~(1 << i);
 
-//         if (!visited[next.first])
-//         {
-//             dp[1 << now] = max(dp);
-//             dfs(next.first, dp, visited, graph)
-//         }
-//     }
-// }
+            for (int k = 0; k < v; k++)
+            {
+                if ((before & (1 << k)) && graph.count({k, i}) && dp[before][k] != INT_MAX)
+                    dp[msk][i] = min(dp[msk][i], dp[before][k] + graph[{k, i}]);
+            }
+        }
+    }
+
+    int ans = INT_MAX;
+
+    for (int i = 0; i < v; i++)
+        if (graph.count({i, start}) && dp[(1 << v) - 1][i] != INT_MAX)
+            ans = min(ans, dp[(1 << v) - 1][i] + graph[{i, start}]);
+
+    return ans;
+}
 
 int main()
 {
     int v, e;
     cin >> v >> e;
 
-    vector<map<int, int>> graph(e);
+    map<pair<int, int>, int> graph;
 
     for (int i = 0; i < e; i++)
     {
         int s, t, d;
         cin >> s >> t >> d;
-        graph[s][t] = d;
+        graph[{s, t}] = d;
     }
 
-    vector<int> dp(1 << v, 0);
+    int ans = INT_MAX;
 
-    for (int msk = 0; msk < 1 << v; msk++)
-    {
-    }
+    for (int start = 0; start < 1; start++)
+        ans = min(ans, calc_min_diff(start, v, graph));
 
-    // for (int start = 0; start < v; start++)
-    // {
-    //     vector<bool> visited(v, false);
-    //     dfs(start, dp, visited, graph);
-    // }
+    if (ans != INT_MAX)
+        cout << ans << endl;
+    else
+        cout << -1 << endl;
 
     return 0;
 }
