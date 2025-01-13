f85fcbd58c30f77c24f9bdbaa47635fe2a1fa482
seicham <onigirimentai422@gmail.com>
2025-01-13 10:08:13 +0900

    2025_01_12

---
+++ contest/abc368/d/main.cpp
@@ -0,0 +1,70 @@
+// https://atcoder.jp/contests/abc368/tasks/abc368_d
+
+// 分からなかった
+// 頂点を残す為の条件は？
+
+// 2025_01_13もう一度
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+
+void dfs()
+{
+}
+
+int main()
+{
+
+    int k, n;
+    cin >> n >> k;
+
+    vector<vector<int>> to(n);
+
+    rep(i, n - 1)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        a--, b--;
+
+        to[a].push_back(b);
+        to[b].push_back(a);
+    }
+
+    vector<int> v(k);
+
+    vector<bool> selected(n, false);
+    rep(i, k)
+    {
+        cin >> v[i];
+        v[i]--;
+
+        selected[v[i]] = true;
+    }
+
+    vector<int> num(n);
+
+    auto dfs = [&](auto dfs, int now, int p = -1) -> void
+    {
+        if (selected[now])
+            num[now]++;
+        for (int next : to[now])
+        {
+
+            if (next == p)
+                continue;
+            dfs(dfs, next, now);
+            num[now] += num[next];
+        }
+    };
+
+    dfs(dfs, v[0]);
+
+    int ans = 0;
+    rep(i, n) if (num[i] > 0) ans++;
+
+    cout << ans << endl;
+
+    return 0;
+}
--- contest/abc369/d/main.cpp
+++ contest/abc369/d/main.cpp
@@ -1,3 +1,9 @@
+// https://atcoder.jp/contests/abc369/tasks/abc369_d
+
+// 解けたが、コードの書き方が悪かった
+
+// 2025_01_13もう一度
+
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
@@ -7,24 +13,67 @@ int main()
 {
     int n;
     cin >> n;
+
     vector<int> a(n);
+
     rep(i, n) cin >> a[i];
 
-    vector<vector<ll>> dp(n + 1, vector<ll>(2, 0));
+    const ll INF = 1e18;
+    vector<vector<ll>> dp(n + 1, vector<ll>(2, -INF));
 
-    for (int i = 1; i <= n; i++)
+    dp[0][0] = 0;
+
+    rep(i, n)
     {
-        if (i > 1)
+        rep(j, 2)
         {
-            dp[i][0] = max(dp[i][0], dp[i - 1][0]);
-            dp[i][0] = max(dp[i][0], dp[i - 1][1] + 2 * a[i - 1]);
+            int ni = i + 1;
+            {
+                int nj = (j + 1) % 2;
+                int X = nj == 0 ? 2 * a[i] : a[i];
+                dp[ni][nj] = max(dp[ni][nj], dp[i][j] + X);
+            }
+            {
+                int nj = j;
+                dp[ni][nj] = max(dp[ni][nj], dp[i][j]);
+            }
         }
-
-        dp[i][1] = max(dp[i][1], dp[i - 1][0] + a[i - 1]);
-        dp[i][1] = max(dp[i][1], dp[i - 1][1]);
     }
 
     ll ans = max(dp[n][0], dp[n][1]);
+
     cout << ans << endl;
+
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+// using ll = long long;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> a(n);
+//     rep(i, n) cin >> a[i];
+
+//     vector<vector<ll>> dp(n + 1, vector<ll>(2, 0));
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         if (i > 1)
+//         {
+//             dp[i][0] = max(dp[i][0], dp[i - 1][0]);
+//             dp[i][0] = max(dp[i][0], dp[i - 1][1] + 2 * a[i - 1]);
+//         }
+
+//         dp[i][1] = max(dp[i][1], dp[i - 1][0] + a[i - 1]);
+//         dp[i][1] = max(dp[i][1], dp[i - 1][1]);
+//     }
+
+//     ll ans = max(dp[n][0], dp[n][1]);
+//     cout << ans << endl;
+//     return 0;
+// }
