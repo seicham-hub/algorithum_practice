ff013c72ca0753d273c4e1ced06c3335ce4c6172
seicham <onigirimentai422@gmail.com>
2025-02-19 15:09:12 +0900

    2025_02_14

---
+++ contest/abc334/c/main.cpp
@@ -0,0 +1,93 @@
+// https://atcoder.jp/contests/abc334/tasks/abc334_c
+
+// 分からなかった...
+// 2025_02_20もう一度
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+using ll = long long;
+using P = pair<int, int>;
+
+int main()
+{
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> cnt(n, 2);
+
+    rep(i, k)
+    {
+        int a;
+        cin >> a;
+        a--;
+        cnt[a]--;
+    }
+
+    vector<int> x;
+    rep(i, n) rep(j, cnt[i]) x.push_back(i);
+
+    n = x.size();
+
+    if (n % 2 == 0)
+    {
+        int ans = 0;
+        rep(i, n / 2) ans += x[i * 2 + 1] - x[i * 2];
+        cout << ans << endl;
+    }
+    else
+    {
+        int now = 0;
+        rep(i, n / 2) now += x[i * 2 + 2] - x[i * 2 + 1];
+        int ans = now;
+        for (int i = 2; i < n; i += 2)
+        {
+            now += x[i - 1] - x[i - 2];
+            now -= x[i] - x[i - 1];
+            ans = min(ans, now);
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+// using ll = long long;
+// using P = pair<int, int>;
+
+// int main()
+// {
+//     int n, k;
+//     cin >> n >> k;
+
+//     vector<int> a(k);
+
+//     rep(i, k) cin >> a[i];
+
+//     if (k % 2 == 1)
+//     {
+//         ll front = 0, back = 0;
+//         for (int i = 0; i + 1 <= k - 2; i = i + 2)
+//             front += a[i + 1] - a[i];
+
+//         for (int i = 1; i + 1 <= k - 1; i = i + 2)
+//             back += a[i + 1] - a[i];
+
+//         cout << min(front, back) << endl;
+//     }
+//     else
+//     {
+//         ll ans = 0;
+//         for (int i = 0; i + 1 <= k - 1; i = i + 2)
+//             ans = a[i + 1] - a[i];
+
+//         cout << ans << endl;
+//     }
+
+//     return 0;
+// }
--- contest/abc336/c/main.cpp
+++ contest/abc336/c/main.cpp
@@ -16,21 +16,16 @@ int main()
     cin >> n;
     n--;
 
-    ll dx[5] = {0, 2, 4, 6, 8};
-
     string ans = "";
+    unordered_map<int, int> mp = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
 
-    if (n == 0)
+    while (true)
     {
-        cout << 0 << endl;
-        return 0;
-    }
-
-    while (n > 0)
-    {
-
-        ans += (to_string(dx[n % 5]));
+        ans += to_string(mp[n % 5]);
         n = n / 5;
+
+        if (n == 0)
+            break;
     }
 
     reverse(ans.begin(), ans.end());
@@ -39,3 +34,40 @@ int main()
 
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+// using ll = long long;
+// using P = pair<int, int>;
+
+// int main()
+// {
+
+//     ll n;
+//     cin >> n;
+//     n--;
+
+//     ll dx[5] = {0, 2, 4, 6, 8};
+
+//     string ans = "";
+
+//     if (n == 0)
+//     {
+//         cout << 0 << endl;
+//         return 0;
+//     }
+
+//     while (n > 0)
+//     {
+
+//         ans += (to_string(dx[n % 5]));
+//         n = n / 5;
+//     }
+
+//     reverse(ans.begin(), ans.end());
+
+//     cout << ans << endl;
+
+//     return 0;
+// }
