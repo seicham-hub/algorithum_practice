94154cd9e56bf1614605a42bf5503363e8fc8b29
seiga <splakuba@gmail.com>
2025-03-12 15:14:21 +0900

    2025_03_11

---
+++ contest/abc321/c/main.cpp
@@ -0,0 +1,86 @@
+// https://atcoder.jp/contests/abc321/tasks/abc321_c
+
+// 解けたが、時間かかりすぎ
+
+// 2025_03_12もう一度
+
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < n; ++i)
+using ll = long long;
+
+int main()
+{
+    int k;
+    cin >> k;
+
+    vector<ll> ans;
+
+    rep(s, 1 << 10)
+    {
+
+        ll x = 0;
+        for (int i = 9; i >= 0; i--)
+        {
+            if (s & 1 << i)
+                x = x * 10 + i;
+        }
+
+        if (x == 0)
+            continue;
+        ans.push_back(x);
+    }
+
+    sort(ans.begin(), ans.end());
+
+    cout << ans[k - 1] << endl;
+
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+// using ll = long long;
+
+// int main()
+// {
+
+//     vector<ll> ans;
+
+//     auto dfs = [&](auto dfs, int &digit, string accum)
+//     {
+//         if (accum.size() == digit)
+//         {
+//             ans.push_back(stoll(accum));
+//             return;
+//         }
+
+//         int now = accum.back() - '0';
+//         if (now == 0)
+//             return;
+
+//         while (now > 0)
+//         {
+//             now--;
+//             dfs(dfs, digit, accum + to_string(now));
+//         }
+//     };
+
+//     rep(i, 10)
+//     {
+//         int digit = i + 1;
+
+//         rep(j, 10)
+//             dfs(dfs, digit, to_string(j));
+//     }
+
+//     sort(ans.begin(), ans.end());
+
+//     ll k;
+//     cin >> k;
+
+//     cout << ans[k] << endl;
+
+//     return 0;
+// }
--- contest/abc395/c/main.cpp
+++ contest/abc395/c/main.cpp
@@ -1,3 +1,8 @@
+// https://atcoder.jp/contests/abc395/tasks/abc395_c
+// 解けたが時間かかった
+
+// 2025_03_11もう一度
+
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
@@ -13,29 +18,78 @@ int main()
 
     rep(i, n) cin >> a[i];
 
-    vector<vector<int>> mp(1e6 + 1, vector<int>());
+    vector<int> cnt(2e6);
 
-    rep(i, n)
+    int r = 0;
+
+    const int INF = 2e6;
+    int mult = 0;
+    int ans = INF;
+    rep(l, n)
     {
 
-        mp[a[i]].push_back(i);
-    }
-
-    int ans = 2e6;
-    rep(i, mp.size())
-    {
-        rep(j, mp[i].size())
+        while (r < n && mult == 0)
         {
-            if (j == 0)
-                continue;
-            ans = min(ans, mp[i][j] - mp[i][j - 1] + 1);
+            cnt[a[r]]++;
+
+            if (cnt[a[r]] == 2)
+                mult++;
+            r++;
         }
+        if (mult == 0)
+            break;
+
+        ans = min(ans, r - l);
+        if (cnt[a[l]] == 2)
+            mult--;
+        cnt[a[l]]--;
     }
 
-    if (ans == 2e6)
+    if (ans == INF)
         cout << -1 << endl;
     else
         cout << ans << endl;
-
     return 0;
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// #define rep(i, n) for (int i = 0; i < n; ++i)
+// using ll = long long;
+// using P = pair<int, int>;
+
+// int main()
+// {
+
+//     int n;
+//     cin >> n;
+//     vector<int> a(n);
+
+//     rep(i, n) cin >> a[i];
+
+//     vector<vector<int>> mp(1e6 + 1, vector<int>());
+
+//     rep(i, n)
+//     {
+
+//         mp[a[i]].push_back(i);
+//     }
+
+//     int ans = 2e6;
+//     rep(i, mp.size())
+//     {
+//         rep(j, mp[i].size())
+//         {
+//             if (j == 0)
+//                 continue;
+//             ans = min(ans, mp[i][j] - mp[i][j - 1] + 1);
+//         }
+//     }
+
+//     if (ans == 2e6)
+//         cout << -1 << endl;
+//     else
+//         cout << ans << endl;
+
+//     return 0;
+// }
